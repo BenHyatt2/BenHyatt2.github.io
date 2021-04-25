56bb3a9ec30821e9c6d36f6e6d417fc1fc85a5a7
BenHyatt2 <83130338+BenHyatt2@users.noreply.github.com>
2021-04-25 10:07:36 -0400

    Update twitterBot.py

--- twitterBot.py
+++ twitterBot.py
@@ -22,3 +22,15 @@ for child in children:
     str=str+child.getText()
 str=" ".join(str.split()) #Remove extra tabs and whitespace
 print(str)
+
+import tweepy
+
+# Authenticate to Twitter
+auth = tweepy.OAuthHandler("CONSUMER_KEY", "CONSUMER_SECRET")
+auth.set_access_token("ACCESS_TOKEN", "ACCESS_TOKEN_SECRET")
+
+# Create API object
+api = tweepy.API(auth)
+
+# Create a tweet
+api.update_status(gtp3Output)
